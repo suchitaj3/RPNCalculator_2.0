3355412343628d71fd830d05b5e23132951ebcb0
suchitaj3 <suchitaj3@gmail.com>
2016-06-17 14:46:18 +0800

    report generation html

--- src/test/java/TestseleniumTestRunner.java
+++ src/test/java/TestseleniumTestRunner.java
@@ -8,7 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/seleniumTestGoogle")
 //@CucumberOptions(features="src/test/resources/seleniumTestGoogle",glue="packageName",tags="~@pending",format = {"pretty", "html:target\cucumber" })
-//~ mean not to execute, without ~ its to execute with this tag only.
+//~ mean not to execute, without ~ its to execute with this tag only..
+
 public class TestseleniumTestRunner {
 
 }
