bb63e6e32c43ae44447cdd30009222fc2ad7a726
suchitaj3 <suchitaj3@gmail.com>
2016-06-17 14:40:02 +0800

    glue, tags keyword use

--- src/test/java/TestseleniumTestRunner.java
+++ src/test/java/TestseleniumTestRunner.java
@@ -7,6 +7,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/seleniumTestGoogle")
+//@CucumberOptions(features="src/test/resources/seleniumTestGoogle",glue="packageName",tags="~@pending")
+//~ mean not to execute, without ~ its to execute with this tag only.
 public class TestseleniumTestRunner {
 
 }
